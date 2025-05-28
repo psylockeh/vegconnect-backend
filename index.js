e2fb396afa6b63819a11147eb527cbdd88f06185
Debora Nogueira <deboran.silva@hotmail.com>
2025-05-27 22:11:39 -0300

    fix(index.js): corrige duplicidade entre app.js e index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Rotas
-app.use("/auth", require("./routes/authRoutes"));
-app.use("/usuario", require("./routes/usuarioRoutes"));
-
-// Início do servidor
-const PORT = process.env.PORT || 28147;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Servidor rodando na porta ${PORT}`)
-);
-
-app.get("/ping", (req, res) => {
-  res.send("pong");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
