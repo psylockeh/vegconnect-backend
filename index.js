18f3688a3997026e075a4c12ca4481b4ed04bf0c
Debora Nogueira <deboran.silva@hotmail.com>
2025-05-27 23:14:34 -0300

    fix(index.js): corrige duplicidade entre app.js e index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const PORT = process.env.PORT || 28147;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Servidor rodando na porta ${PORT}`)
+);
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
