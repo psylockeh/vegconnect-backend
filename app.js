5d78ab3df113e45ccd63242bc63fd8b8ec12d555
Debora Nogueira <deboran.silva@hotmail.com>
2025-06-24 19:02:05 -0300

    fix: ajusta rotas de curtir

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const googleRoutes = require("./routes/googleRoutes");
 const comentarioRoutes = require("./routes/comentarioRoutes");
 const curtidaRoutes = require("./routes/curtidaRoutes");
 const repostRoutes = require("./routes/repostRoutes");
-const postagemRoutes = require("./routes/postagemRoutes");
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(uploadRoutes);
 app.use("/externo/google", googleRoutes);
 app.use("/comentarios", comentarioRoutes);
 app.use("/curtidas", curtidaRoutes);
-app.use("/postagens", postagemRoutes);
 app.use("/reposts", repostRoutes);
 
 module.exports = app;
